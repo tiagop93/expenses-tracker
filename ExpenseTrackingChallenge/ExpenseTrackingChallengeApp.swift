fe63714c7328d32ea45d872855d778cbb93f4e34
Tiago Pereira <tiago_pereira_93@hotmail.com>
2025-06-12 13:25:48 +0100

    Initial Commit

---
+++ ExpenseTrackingChallenge/ExpenseTrackingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackingChallengeApp.swift
+//  ExpenseTrackingChallenge
+//
+//  Created by Tiago Pereira on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
